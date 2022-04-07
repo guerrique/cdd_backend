10d320c065dfd1e39a9cebb8257720181c89ed6d
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2022-04-07 15:49:18 +0800

    misplaced searchable.rb, should be in app/models/concerns

--- app/controllers/search_entries_controller.rb
+++ app/controllers/search_entries_controller.rb
@@ -1,5 +1,7 @@
 class SearchEntriesController < ApplicationController
   def index
     @search_entries = SearchEntry.where("title LIKE ?", "%#{params[:query]}%") if params[:query]
+
+    render json: @search_entries
   end
 end
